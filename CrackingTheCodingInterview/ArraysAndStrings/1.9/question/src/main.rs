ad04ee9df23a1941c9a50c18fd5ac4c58fc1acb9
Your Name <quadekirby@gmail.com>
2025-10-01 21:31:56 -0400

    minor update

--- CrackingTheCodingInterview/ArraysAndStrings/1.9/question/src/main.rs
+++ CrackingTheCodingInterview/ArraysAndStrings/1.9/question/src/main.rs
@@ -12,6 +12,8 @@ pub fn is_substring(val1: &str,val2: &str) -> Result<bool,String> {
 
     let tmp = val2.to_string() + val2;
 
+
+    let temp = "nothing";
     if tmp.contains(val1) {
         Ok(true)
     }
