0217e632fa91f90bb74eb7fa040b3ef7234cab20
Your Name <quadekirby@gmail.com>
2025-10-09 22:48:57 -0400

    minor change

--- rust-template/question/src/tests.rs
+++ rust-template/question/src/tests.rs
@@ -16,3 +16,44 @@ mod tests {
         assert_eq!(value.unwrap(), 5.0);
     }
 }
+//https://geo-ant.github.io/blog/2022/implementing-parallel-iterators-rayon/
+
+struct ParDataIter<'a> {
+  data_slice : &'a [Data]
+}
+
+impl<'a> ParallelIterator for ParDataIter<'a> {
+    type Item = &'a Data;
+
+    fn drive_unindexed<C>(self, consumer: C) -> C::Result
+    where
+        C: UnindexedConsumer<Self::Item> {
+        bridge(self,consumer)
+    }
+
+    fn opt_len(&self) -> Option<usize> {
+      Some(self.len())
+    }
+}
+
+impl<'a> IndexedParallelIterator for ParDataIter<'a> {
+    fn with_producer<CB: ProducerCallback<Self::Item>>(
+        self,
+        callback: CB,
+    ) -> CB::Output {
+        todo!()
+    }
+
+    fn drive<C: Consumer<Self::Item>>(self, consumer: C) -> C::Result {
+        bridge(self,consumer)
+    }
+
+    fn len(&self) -> usize {
+        self.data_slice.len()
+    }
+}
+
+struct DataProducer<'a> {
+  data_slice : &'a [Data],
+}
+
