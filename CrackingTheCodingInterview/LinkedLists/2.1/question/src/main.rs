ca0cd169abaa703d0c791f1db87a39885b059cf4
Your Name <quadekirby@gmail.com>
2025-10-23 21:42:25 -0400

    update to 2.1

---
+++ CrackingTheCodingInterview/LinkedLists/2.1/question/src/main.rs
@@ -0,0 +1,18 @@
+
+pub mod tests;
+use std::collections::LinkedList;
+fn main() {
+    println!("Hello, world!");
+    println!("{}", is_unique("value".to_string()).unwrap());
+}
+
+//Assumming ASCII
+pub fn is_unique(value: String) -> Result<bool, String> {
+    use std::collections::LinkedList;
+
+    let list = LinkedList::from([1, 2, 3]);
+
+    return Ok(true);
+    
+
+}
