37f9d3e70cc051cf50a2f6928fbcedad6fc05993
Your Name <quadekirby@gmail.com>
2025-11-21 23:31:44 -0500

    added a few more steps to the watershed process

--- watershed/src/main.rs
+++ watershed/src/main.rs
@@ -1,12 +1,11 @@
-use image::{ImageBuffer, Luma, open};
-use imageproc::{contrast::{ThresholdType, otsu_level, threshold}, gray_image};
+use std::collections::HashMap;
+
+use image::{ImageBuffer, Luma, Rgb, open};
+use imageproc::{contrast::{ThresholdType, otsu_level, threshold}, distance_transform::{Norm, distance_transform}, morphology::{Mask, dilate, grayscale_open}, region_labelling::connected_components};
+use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
-
-    let image = gray_image!(
-    10, 80, 20;
-    50, 90, 70);
+    println!("Starting");
 
     let img = open("image.png").expect("Failed to open image");
 
@@ -14,8 +13,59 @@ fn main() {
     let gray_img: ImageBuffer<Luma<u8>, Vec<u8>> = img.to_luma8();
     let _ = gray_img.save("gray.png");
 
-    let result = threshold(&gray_img, 161, ThresholdType::BinaryInverted);
-    let _ = result.save("threshold.png");
+    // find threshold level
+    let _otsu = otsu_level(&gray_img);
+
+    // apply threshold level
+    let thres = threshold(&gray_img, 161, ThresholdType::BinaryInverted);
+    let _ = thres.save("threshold.png");
+
+    // apply open morph
+    let morph = grayscale_open(&thres, &Mask::square(3));
+    let _ = morph.save("morph.png");
+
+    // sure ground
+    let sure = dilate(&morph, Norm::L2, 3);
+    let _ = sure.save("sure.png");
+
+    // apply dilate
+    let distance = distance_transform(&morph, Norm::L2, imageproc::distance_transform::DistanceFrom::Background);
+    let _ = distance.save("distance.png");
+
+    let max = distance.iter().fold(std::u8::MIN, |a,b| a.max(*b));
+    // foreground
+    let foreground = threshold(&distance, max/2, ThresholdType::Binary);
+    let _ = foreground.save("foreground.png");
+    
+
+    let background_color = Luma([155u8]);
+    let connected = connected_components(&foreground, imageproc::region_labelling::Connectivity::Eight, background_color);
+
+    let mut rgb_image = ImageBuffer::new(connected.width(), connected.height());
+
+    let mut color_map: HashMap<u32, [u8; 3]> = HashMap::new();
+    color_map.insert(33, [0,0,0]);
+
+    let mut rng = rand::rng();
+
+
+    for i in 1..30 { // Iterates 1, 2, 3, 4
+            color_map.insert(i, [rng.random_range(1..=254),rng.random_range(1..=254),rng.random_range(1..=254)]);
+        }
+
+    // Iterate through Luma pixels and convert to RGB
+    for (x, y, pixel) in connected.enumerate_pixels() {
+        let luma_value = pixel[0]; // Luma pixels have one channel
+
+        // Create an Rgb pixel where R, G, and B are all the luma value
+        let rgb_pixel: Rgb<u8> = Rgb(*color_map.get(&pixel[0]).unwrap());
+
+        // Set the pixel in the new RGB image
+        rgb_image.put_pixel(x, y, rgb_pixel);
+    }
+    let _ = rgb_image.save("connected.png");
+
+
 
     //https://www.geeksforgeeks.org/computer-vision/image-segmentation-with-watershed-algorithm-opencv-python/
     
